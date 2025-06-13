c7595c369c286eb13f0c7753be54aa32728bffc9
Rulf <lazaarx9@gmail.com>
2025-06-13 22:17:33 +0530

    Update status.py
    Signed-off-by: Rulf <lazaarx9@gmail.com>

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -42,13 +42,13 @@ async def task_status(_, message):
     if count == 0:
         currentTime = get_readable_time(time() - bot_start_time)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        msg = f"""〶 <b><i>No Active Bot Tasks!</i></b>
+        msg = f"""〶 <b><i>Nᴏ Aᴄᴛɪᴠᴇ Dᴏᴡɴʟᴏᴀᴅs ! 🗑️</i></b>
 │
 ┖ <b>NOTE</b> → <i>Each user can get status for his tasks by adding "me" or user_id like "1234xxx" after cmd: /{BotCommands.StatusCommand[0]} me or /{BotCommands.StatusCommand[1]} me</i>
 
-⌬ <b><u>Bot Stats</u></b>
-┟ <b>CPU</b> → {cpu_percent()}% | <b>F</b> → {free} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%]
-┖ <b>RAM</b> → {virtual_memory().percent}% | <b>UP</b> → {currentTime}
+⌬ <b><u>Bᴏᴛ Sᴛᴀᴛs...</u></b>
+┟ <b>🖥️ Cᴘᴜ</b> → {cpu_percent()}% | <b>💿 F</b> → {free} [{round(100 - disk_usage(DOWNLOAD_DIR).percent, 1)}%]
+┖ <b>🎮 Rᴀᴍ</b> → {virtual_memory().percent}% | <b>🚀 Uᴘᴛɪᴍᴇ</b> → {currentTime}
 """
         reply_message = await send_message(message, msg)
         await auto_delete_message(message, reply_message)
