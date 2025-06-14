7e4bfb8368523c1dc57650b79599690e6dd80ea4
Rulf <lazaarx9@gmail.com>
2025-06-14 14:27:40 +0530

    Update restart.py
    Signed-off-by: Rulf <lazaarx9@gmail.com>

--- bot/modules/restart.py
+++ bot/modules/restart.py
@@ -38,8 +38,8 @@ async def restart_bot(_, message):
 @new_task
 async def restart_sessions(_, message):
     buttons = button_build.ButtonMaker()
-    buttons.data_button("Yᴇs!", "sessionrestart confirm")
-    buttons.data_button("Nᴏ!", "sessionrestart cancel")
+    buttons.data_button("Yᴇs ✅", "sessionrestart confirm")
+    buttons.data_button("Nᴏ ❌", "sessionrestart cancel")
     button = buttons.build_menu(2)
     await send_message(
         message,
@@ -50,7 +50,7 @@ async def restart_sessions(_, message):
 
 async def send_incomplete_task_message(cid, msg_id, msg):
     try:
-        if msg.startswith("⌬ <b><i>Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅</i></b>"):
+        if msg.startswith("<b><i>Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅</i></b>"):
             await TgClient.bot.edit_message_text(
                 chat_id=cid,
                 message_id=msg_id,
@@ -81,7 +81,7 @@ async def restart_notification():
     if Config.INCOMPLETE_TASK_NOTIFIER and Config.DATABASE_URL:
         if notifier_dict := await database.get_incomplete_tasks():
             for cid, data in notifier_dict.items():
-                msg = f"""⌬ <b><i>{"Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅" if cid == chat_id else "Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ! ✅"}</i></b>
+                msg = f"""<b><i>{"Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅" if cid == chat_id else "Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ! ✅"}</i></b>
 ┟ <b>Dᴀᴛᴇ:</b> {now.strftime("%d/%m/%y")}
 ┠ <b>Tɪᴍᴇ:</b> {now.strftime("%I:%M:%S %p")}
 ┠ <b>TɪᴍᴇZᴏɴᴇ:</b> Asia/Kolkata
@@ -101,7 +101,7 @@ async def restart_notification():
             await TgClient.bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=msg_id,
-                text=f"""⌬ <b><i>Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ!</i></b>
+                text=f""<b><i>Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅</i></b>
 ┟ <b>Dᴀᴛᴇ:</b> {now.strftime("%d/%m/%y")}
 ┠ <b>Tɪᴍᴇ:</b> {now.strftime("%I:%M:%S %p")}
 ┠ <b>TɪᴍᴇZᴏɴᴇ:</b> Asia/Kolkata
